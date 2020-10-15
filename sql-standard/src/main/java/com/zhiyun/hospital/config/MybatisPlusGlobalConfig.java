58c80ba21d5e908df62374de55223b428c6fa6c1
zhikuanxiao <zhikuanxiao@91jkys.com>
2020-10-15 18:18:16 +0800

    BlockAttackInnerBoostInterceptor 拦截器修改

--- sql-standard/src/main/java/com/zhiyun/hospital/config/MybatisPlusGlobalConfig.java
+++ sql-standard/src/main/java/com/zhiyun/hospital/config/MybatisPlusGlobalConfig.java
@@ -1,18 +1,23 @@
 package com.zhiyun.hospital.config;
 
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
 import com.zhiyun.hospital.EnableSqlStandard;
+import com.zhiyun.hospital.interceptor.BlockAttackInnerBoostInterceptor;
 import com.zhiyun.hospital.interceptor.CustomerIllegalSQLInterceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.AnnotationUtils;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,10 +28,22 @@ import java.util.Map;
  */
 @Configurable
 @ConditionalOnClass({SqlSessionFactory.class})
-public class MybatisPlusGlobleConfig implements InitializingBean, ApplicationContextAware {
+public class MybatisPlusGlobalConfig implements InitializingBean, ApplicationContextAware {
     private String path = null;
     private ApplicationContext applicationContext;
 
+    /**
+     * 注册所有的Mybatis-Plus的InnerInterceptor
+     * @param list
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    MybatisPlusInterceptor mybatisPlusInterceptor(List<InnerInterceptor> list){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.setInterceptors(list);
+        return mybatisPlusInterceptor;
+    }
     /**
      * lllegal sql interceptor
      *
@@ -37,6 +54,11 @@ public class MybatisPlusGlobleConfig implements InitializingBean, ApplicationCon
         return new CustomerIllegalSQLInterceptor(path);
     }
 
+    @Bean
+    public BlockAttackInnerBoostInterceptor blockAttackInnerBoostInterceptor(){
+        return new BlockAttackInnerBoostInterceptor();
+    }
+    
     @Override
     public void afterPropertiesSet() throws Exception {
         Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(EnableSqlStandard.class);
